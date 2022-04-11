72b58b3bec64329d861dd3d43c95754ea769c03c
Amanda Bahadori <abahador@ucsc.edu>
2022-04-11 10:16:56 -0700

    explosions

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -16,4 +16,8 @@ class Spaceship extends Phaser.GameObjects.Sprite {
             this.x = game.config.width;
         }
     }
+    // position reset
+    reset () {
+        this.x = game.config.width;
+    }
 }
